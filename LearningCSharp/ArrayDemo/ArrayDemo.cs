66f082e00558363df5321f4027e092592dba25ff
Kaung Min Htet <kgminhtet.dev@gmail.com>
2026-02-21 11:38:46 +0630

    ArrayDemo

---
+++ LearningCSharp/ArrayDemo/ArrayDemo.cs
@@ -0,0 +1,14 @@
+namespace LearningCSharp.ArrayDemo;
+
+public class ArrayDemo
+{
+    public static void Test()
+    {
+        string[] colors = ["red", "green", "blue"];
+
+        foreach (var color in colors)
+        {
+            Console.WriteLine(color);
+        }
+    }
+}
--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using LearningCSharp.ArrayDemo;
+
+ArrayDemo.Test();
+
 Console.WriteLine("Hello, World!");
